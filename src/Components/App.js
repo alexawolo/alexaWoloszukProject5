66deab4c1f1767f997dc86bf71c04d46dc4c8966
Alexa <alexawoloszuk@gmail.com>
2019-09-10 11:00:11 -0400

    updates

--- src/Components/App.js
+++ src/Components/App.js
@@ -20,7 +20,7 @@ class App extends Component {
 
     axios({
       method: 'get',
-      url: 'http://www.nokeynoshade.party/api/queens/all',
+      url: 'https://www.nokeynoshade.party/api/queens/all',
       responseType: 'json',
       params: {
         format: 'json',
@@ -28,19 +28,16 @@ class App extends Component {
       }
 
     }).then((res) => {
-      // console.log(res.data); // outputs array obj of 140 queens
 
-      // clone original array
       const data = [...res.data];
       const randomQueenArray = [];
 
-      // for loop
       for (let i = 0; i < 9; i++){
         let randomNumber = Math.floor(Math.random() * data.length )
         randomQueenArray.push(data[randomNumber])
 
         data.splice(randomNumber, 1);
-        console.log(randomQueenArray);
+        // console.log(randomQueenArray);
 
 
       }
@@ -55,30 +52,16 @@ class App extends Component {
 
   handleClick(event) {
     console.log('clicked!');
+
+
+
   }
-
-//   loadMoreClick(event) {
-//     // instead of getting data from noRepeatQueensArray
-
-//     // Gettin extra 9 queens (for load more)
-//     // for (let i = 0; i < 9; i++){
-//     //   let randomNumber = Math.floor(Math.random() * data.length )
-//     //   extraQueens.push(data[randomNumber])
-
-//     //   data.splice(randomNumber, 1);
-//     //   console.log(extraQueens);
-//     // }
-// q
-//     // concatenate extraQueens and copied this.state.queensArray
-//     // update state (setState) .. make the above queenArray
-//   }
 
   render(){
     return (
       <div className="App">
         <h1>Don't be a drag, just be a queen</h1>
         <h2>Click on the queens below to find out their name and famous quote!</h2>
-        <button className="loadMoreButton">Load More</button>
         <div className="allCards">
           {this.state.queenArray.map((queen) => {
             //parameter is queen, the arguement is the object that exist in the array
@@ -87,8 +70,8 @@ class App extends Component {
               <Queen 
                 img={queen.image_url}
                 click={this.handleClick}
-                // name={queen.name} 
-                // quote={queen.quote}
+                name={queen.name} 
+                quote={queen.quote}
                 key={queen.id}
               />
             )
--- src/Components/Queen.js
+++ src/Components/Queen.js
@@ -11,14 +11,14 @@ class Queen extends Component{
     }
 
     imageError = (e) => {
-        console.log(e.target);
+        // console.log(e.target);
 
         // if runs... this.state.errored === true
         // when loading... 
 
         if (!this.state.errored) {
             this.setState({
-              src: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/e0/SNice.svg/1200px-SNice.svg.png',
+              src: 'https://ih0.redbubble.net/image.222677543.3720/flat,550x550,075,f.u2.jpg',
               errored: true,
             });
         }
@@ -28,10 +28,10 @@ class Queen extends Component{
         return(
             <div className="queenCard">
                 <img src={this.state.src} alt="" onClick={this.props.click} onError={this.imageError}/>
-                {/* <div className="cardContent">
+                <div className="cardContent">
                     <h2 className="queenName">{this.props.name}</h2>
                     <p className="queenQuote">{this.props.quote}</p>
-                </div> */}
+                </div>
             </div>
         );
     }
