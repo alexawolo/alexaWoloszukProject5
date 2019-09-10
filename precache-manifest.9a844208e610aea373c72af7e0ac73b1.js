1acdaa59ff45077ff41124f74a7949e3b27357a2
Alexa <alexawoloszuk@gmail.com>
2019-09-10 10:19:23 -0400

    Updates

---
+++ precache-manifest.9a844208e610aea373c72af7e0ac73b1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a2273f7c07b361de15dd41ee625315fd",
+    "url": "/alexaWoloszukProject5/index.html"
+  },
+  {
+    "revision": "0e5c5c123c90f006611b",
+    "url": "/alexaWoloszukProject5/static/css/main.4945063a.chunk.css"
+  },
+  {
+    "revision": "6fadb1c46c95a134500b",
+    "url": "/alexaWoloszukProject5/static/js/2.b659da45.chunk.js"
+  },
+  {
+    "revision": "0e5c5c123c90f006611b",
+    "url": "/alexaWoloszukProject5/static/js/main.d9b2c75f.chunk.js"
+  },
+  {
+    "revision": "bbdd01bc0c275bfd6fa6",
+    "url": "/alexaWoloszukProject5/static/js/runtime~main.0c0e0b40.js"
+  }
+]);
